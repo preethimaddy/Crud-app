c48a5c3ce7d9dbad07947b6c1904cd4971ccb906
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2023-06-05 18:15:52 +0530

    handleInput updating data

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from "react";
 import {Container,Row,Col,Form,Button, Alert} from "react-bootstrap"
-function HomePage() {
-
+function HomePage(props) {
+  const initialState = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    gender: "",
+  };
+  const [newInput, setNewInput] = useState(initialState);
+  console.log("newInput", newInput);
+  const handleInput =(e) =>{
+e.preventDefault ();
+const { name, value } = e.target;
+    const data = { ...newInput };
+    data[name] = value;
+    setNewInput(data);
+  }
   return (
     <>
    <Container>
@@ -11,7 +25,7 @@ function HomePage() {
       <Col xs={6} md={6}>
       <Form.Group className="mb-3" controlId="formfName">
         <Form.Label>First Name</Form.Label>
-        <Form.Control type="text" name="firstname" placeholder="Enter your firstname" />
+        <Form.Control type="text" name="firstname" placeholder="Enter your firstname" onChange = {handleInput}/>
         <Form.Text className="text-muted">
                       Enter Your First Name
                     </Form.Text>
@@ -20,7 +34,7 @@ function HomePage() {
       <Col xs={6} md={6}>
       <Form.Group className="mb-3" controlId="formlName">
         <Form.Label>Last Name</Form.Label>
-        <Form.Control type="text" name="lastname" placeholder="Enter your lastname" />
+        <Form.Control type="text" name="lastname" placeholder="Enter your lastname" onChange = {handleInput} />
         <Form.Text className="text-muted">
                       Enter Your Last Name
                     </Form.Text>
@@ -32,7 +46,7 @@ function HomePage() {
       <Col xs={6} md={6}>
       <Form.Group className="mb-3" controlId="formbasicemail">
         <Form.Label>Email Address</Form.Label>
-        <Form.Control type="text" name="email" placeholder="Enter your email address" />
+        <Form.Control type="text" name="email" placeholder="Enter your email address" onChange = {handleInput}/>
         <Form.Text className="text-muted">
                       We will Never share your Address to Anyone Else!
                     </Form.Text>
@@ -43,10 +57,12 @@ function HomePage() {
         <Form.Label>Gender</Form.Label>
         <select
                       name="gender"
+                      onChange={handleInput}
                       className="form-select" >
                       <option value="">--select--</option>
                       <option value="Male">Male</option>
                       <option value="Female">Female</option>
+                      
                     </select>
         <Form.Text className="text-muted">
                       Enter Your Last Name
@@ -55,7 +71,7 @@ function HomePage() {
       </Col>
     </Row>
     <Col xs={12} className="text-center">
-    <Button className="btn btn-sucess">Add</Button>
+    <Button className="btn btn-success">Add</Button>
     
     <Button className="btn btn-danger"> Edit</Button></Col>
     </Form>
